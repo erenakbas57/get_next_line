5d96e1f9ef74e1433c1eb08dfdd23527c737b252
Muhammed Eren Akbas <makbas@k1m20s09.42istanbul.com.tr>
2022-12-22 14:34:43 +0300

    125

---
+++ get_next_line_bonus.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: makbas <makbas@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/23 15:02:07 by makbas            #+#    #+#             */
+/*   Updated: 2022/12/22 14:33:20 by makbas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*next_read(char *str)
+{
+	int		i;
+	int		j;
+	char	*next_str;
+
+	i = 0;
+	while (str[i] != '\n' && str[i])
+		i++;
+	if (str[i] == 0)
+	{
+		free(str);
+		return (NULL);
+	}
+	next_str = (char *)malloc(sizeof(char) * (ft_strlen(str) - i));
+	if (!next_str)
+		return (NULL);
+	i++;
+	j = 0;
+	while (str[i])
+		next_str[j++] = str[i++];
+	next_str[j] = 0;
+	free(str);
+	return (next_str);
+}
+
+char	*read_first_line(char *str)
+{
+	char	*new_str;
+	int		i;
+
+	i = 0;
+	if (!str[i])
+		return (NULL);
+	while (str[i] != '\n' && str[i])
+		i++;
+	new_str = malloc(sizeof(char) * (i + 2));
+	if (!new_str)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\n' && str[i])
+	{
+		new_str[i] = str[i];
+		i++;
+	}
+	if (str[i] == '\n')
+		new_str[i++] = '\n';
+	new_str[i] = 0;
+	return (new_str);
+}
+
+char	*read_line(char *str, int fd)
+{
+	char	*line;
+	int		line_i;
+
+	line = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!line)
+		return (NULL);
+	line_i = 1;
+	while (ft_strchr(str, '\n') == 0 && line_i != 0)
+	{
+		line_i = read(fd, line, BUFFER_SIZE);
+		if (line_i == -1)
+		{
+			free(str);
+			free(line);
+			return (0);
+		}
+		line[line_i] = '\0';
+		str = ft_strjoin(str, line);
+	}
+	free(line);
+	return (str);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*str[2048];
+	char		*line;
+
+	if (BUFFER_SIZE <= 0 || fd < 0)
+		return (NULL);
+	str[fd] = read_line(str[fd], fd);
+	if (!str[fd])
+		return (NULL);
+	line = read_first_line(str[fd]);
+	str[fd] = next_read(str[fd]);
+	return (line);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: makbas <makbas@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/23 15:01:53 by makbas            #+#    #+#             */
+/*   Updated: 2022/11/23 15:38:01 by makbas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+char	*read_line(char *str, int fd);
+char	*read_first_line(char *str);
+char	*next_read(char *str);
+
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(char *str, int c);
+char	*ft_strjoin(char *str, char *buff);
+
+#endif
